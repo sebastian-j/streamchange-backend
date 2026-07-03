491ead8d1e3535759ac972cf28abc5ac05f40075
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-03 11:46:54 +0200

    feat: kick chat

--- src/clients/abstract_client.py
+++ src/clients/abstract_client.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Callable
 
 
 class AbstractClient(ABC):
+    def __init__(self, on_message: Callable) -> None:
+        self.on_message = on_message
+
     @abstractmethod
-    async def connect(self, channel: str) -> None:
+    async def connect(self, channel: str | int) -> None:
         pass
 
     @abstractmethod
---
+++ src/config.py
@@ -0,0 +1,4 @@
+KICK_PUSHER_WS = (
+    "wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679"
+    "?protocol=7&client=js&version=8.4.0-rc2&flash=false"
+)
