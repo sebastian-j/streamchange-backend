e4d7ecc4e18728a7451b6c33080dd7d458d90201
Dominik Jurkowski <dominikjurko4@gmail.com>
2026-07-06 15:04:09 +0200

    adding .env

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 KICK_PUSHER_WS = (
     "wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679"
     "?protocol=7&client=js&version=8.4.0-rc2&flash=false"
@@ -5,5 +10,5 @@ KICK_PUSHER_WS = (
 TWITCH_IRC_HOST = "irc.chat.twitch.tv"
 TWITCH_IRC_PORT = 6667
 TWITCH_NICK = "justinfan12345"
-TWITCH_CLIENT_ID = "wfe821ccwpi9b3omk232kwktapw5in"
-TWITCH_CLIENT_SECRET = "gzt45l42tvcjaeon3nx7i31eguumo7"
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
