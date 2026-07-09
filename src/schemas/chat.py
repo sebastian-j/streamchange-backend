d3b98f0a2c47584260d4a587a5ed6fc48221cc21
Dominik Jurkowski <dominikjurko4@gmail.com>
2026-07-09 12:50:45 +0200

    adding emotes handling in internal chat

--- src/schemas/chat.py
+++ src/schemas/chat.py
@@ -2,12 +2,20 @@ from pydantic import BaseModel
 from typing import Optional
 
 
+class MessageFragment(BaseModel):
+    type: str
+    text: Optional[str] = None
+    url: Optional[str] = None
+    code: Optional[str] = None
+
+
 class ChatMessage(BaseModel):
     author: str
     message: str
     color: str
     badges: Optional[list[str]] = None
     subscriber: int
+    fragments: Optional[list[MessageFragment]] = None
 
 
 class StreamData(BaseModel):
